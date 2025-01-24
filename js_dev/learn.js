b2b501e1350f888ec097694c92c9d3b97b67ab78
omondimax <maxwell.omondi1@student.moringaschool.com>
2025-01-24 16:39:15 +0300

    Add files via upload

---
+++ js_dev/learn.js
@@ -0,0 +1,67 @@
+let match = "footall";
+let games = "team";
+let team  = "barcelona";
+
+//array example using
+let teams = ["man united","barcelona"];
+teams.push = ("miami");
+console.log
+
+
+
+function greetings(team)  {
+    console.log("hello",team)
+}
+
+greetings(name1)
+
+console.log(name.length);
+
+//  anonymous
+let anon = function() {
+    console.log("an anonymous function");}
+
+    (
+
+        let annon =
+    )
+
+    //{}
+
+    // annon()
+
+
+    let teams = ["man u","barca","leverkusen"];
+
+    // for(let i = 0; i< teams.length;i++) {
+        //console.logs(teams[i]);
+    //  }
+    
+    teams.forEach(function(team){
+        let club = team.toUpperCase();
+         console.log(club);
+        });  
+        
+        // for (let team of teams) {
+        //     console.log(team);  
+        //}
+
+        let uppercase teams = teams.map(function(team){
+            return team.toUpperCase();
+        })
+
+        // console.log(uppercase_teams);
+
+
+//function sum(a+b) {
+  //  console.log(a+b);
+//}
+
+//let total = sum(10,10)
+//console.log(total);
+
+let player = 10;
+
+if (player <= 15) {
+    console.log("we are champions");
+}
